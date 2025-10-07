002a4ebdc66e6fef4e79fcb4d0d6e2839be4ac4d
Siddharth Ajith <siddharthajith1717@gmail.com>
2025-10-07 23:17:40 +0530

    day 7 : still on mergesort :(

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -1,47 +1,70 @@
 package sorting;
 
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 //todo: learn this better: revise
 public class MergeSort {
-    void mergeSort(int[] arr, int low, int high) {
-        if ( low >= high) return ;
-        int mid = (low + high ) / 2 ;
-        mergeSort(arr, low, mid) ;
-        mergeSort(arr, mid+1, high) ;
-        merge(arr, low, mid, high);
+    void mergesort(int[ ] arr , int low , int high) {
+        if ( low == high ) return ;
+        int mid = ( low + high ) / 2 ;
+        mergesort(arr, low, mid );
+        mergesort(arr, mid + 1 , high );
+        merge(arr, low, mid, high ) ;
+    }
+
+
+    void merge(int[] arr, int low, int mid , int high ) {
+        int l = low;
+        int r = mid + 1;
+        int k = 0;
+        int[] temp = new int[high - low + 1];
+        while (l <= mid && r <= high) {
+            if (arr[l] < arr[r]) {
+                temp[k++] = arr[l++];
+            } else temp[k++] = arr[r++];
+
+        }
+        while (l <= mid) {
+            temp[k++] = arr[l++];
+        }
+
+        while (r <= high) {
+            temp[k++] = arr[r++];
+        }
+        for (int i = 0; i < temp.length; i++) {
+            arr[i + low] = temp[i];
+        }
         System.out.println(Arrays.toString(arr));
     }
 
-    int[] merge(int[] arr, int low, int mid, int high) {
-        int s1 = low;
-        int s2 = mid + 1;
-        int k = 0;
-        int[] temp = new int[high - low + 1];
-        while (s1 <= mid && s2 <= high) {
-            if (arr[s1] < arr[s2]) {
-                temp[k++] = arr[s1++];
-            } else {
-                temp[k++] = arr[s2++];
-            }
-        }
-        while (s1 <= mid) {
-            temp[k++] = arr[s1++];
-        }
-
-        while (s2 <= high) {
-            temp[k++] = arr[s2++];
-        }
-        for (int x = 0; x < temp.length; x++) {
-            arr[low + x] = temp[x];
-        }
-        return arr;
-    }
 
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
-        mergeSort.mergeSort(new int[]{38, 27, 43, 3, 9, 82, 10}, 0, 6);
+        mergeSort.mergesort(new int[]{38, 27, 43, 3, 9, 82, 10}, 0, 6);
     }
+//    l = 0 mid = 3, high = 6
+//    l = 0 mid = 1, high  = 3
+//    temp = high - low + 1
+//    arr[l + i ] = temp[i];
+//
+//    l = 0
+//    arr[0] = temp[0]
+//    arr[1] =  temp [i]
+//    arr[2] = temp[2]
+//    arr[3] = temp[3]
+//
+//
+//    l = 4
+//    mid = 5
+//    high = 6
+//    temp = [high - low + 1]
+//    temp = [3]
+//    for i : temp
+//    arr[low + i] = temp
+//    arr[4] = temp[0]
+//    arr[5] = temp[1]
+//    arr[6] = temp[2]
 }
