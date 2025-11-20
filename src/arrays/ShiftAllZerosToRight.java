c0e8e74c99128257726197bc6e5da1701bb9569f
Siddharth Ajith <siddharthajith1717@gmail.com>
2025-11-20 20:36:43 +0530

    shift zeros to right

--- src/arrays/ShiftAllZerosToRight.java
+++ src/arrays/ShiftAllZerosToRight.java
@@ -1,4 +1,39 @@
 package arrays;
 
 public class ShiftAllZerosToRight {
+
+    //tc : o(n^2)
+    //sc : o(1)
+    public void shiftZeros(int[] arr) {
+        for (int i = 0 ; i < arr.length ; i ++){
+            if (arr[i] == 0 ){
+                int temp = arr[i];
+                for(int j = i ; j < arr.length - 1; j ++){
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length-1] = temp;
+            }
+        }
+    }
+
+    public void shiftZerosOptimized(int arr[]){
+        int nonZeroIndex = 0 ;
+        for(int i = 0 ; i < arr.length; i ++ ){
+            if(arr[i]  != 0){
+                arr[nonZeroIndex++] = arr[i];
+            }
+        }
+        while(nonZeroIndex < arr.length){
+            arr[nonZeroIndex] = 0;
+            nonZeroIndex++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ShiftAllZerosToRight shiftAllZerosToRight = new ShiftAllZerosToRight();
+        int[] arr = {0, 1, 0, 3, 12};
+        shiftAllZerosToRight .shiftZerosOptimized(arr);
+        for ( int i : arr) System.out.print(i + " ");
+    }
 }
